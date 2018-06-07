35861c8f41723b6b654524f9fdd819938a462912
Verta-master <vertyagina@gmail.com>
2018-06-07 15:00:45 +0600

    Base

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Mobile menu
+$('.menu-top__btn').click(function() {
+  $('.menu-top__list').toggleClass('menu-top__list--show');
+  $('.menu-top').toggleClass('menu-top--opened');
+});
+
+$('.menu__btn').click(function() {
+  $('.menu__list').slideToggle();
+  $('.menu').toggleClass('menu--opened');
+});
+
+$('.footer__btn').click(function() {
+  $(this).next().slideToggle();
+})
+
+//City choice
+$('.header__btn').click(function() {
+  $('.header__list').slideToggle();
+})
